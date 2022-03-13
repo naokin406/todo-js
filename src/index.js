9e4325dfcf1518c3fe83095a6f6a9c7416777ddf
naokin406 <101477481+naokin406@users.noreply.github.com>
2022-03-13 09:49:10 +0900

    TODOアプリの作成

--- src/index.js
+++ src/index.js
@@ -1,10 +1,82 @@
 import "./styles.css";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const onClickAdd = () => {
+  // input
+  const inputText = document.getElementById("add-text").value;
+  document.getElementById("add-text").value = "";
+  createFromInCompleteList(inputText);
+};
+
+// 未完了リストの削除
+const deleteFromInCompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
+
+// 未完了リストの追加
+const createFromInCompleteList = (text) => {
+  // div
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // li
+  const li = document.createElement("li");
+  li.innerText = text;
+
+  // 完了
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    // 未完了リストから削除
+    deleteFromInCompleteList(completeButton.parentNode);
+
+    // 完了　リストに追加
+    const addTarget = completeButton.parentNode;
+    const text = addTarget.firstElementChild.innerText;
+
+    // divを初期化
+    addTarget.textContent = null;
+
+    // li
+    const li = document.createElement("li");
+    li.innerText = text;
+
+    // 戻すボタン
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻る";
+    backButton.addEventListener("click", () => {
+      // リストから削除
+      const deleteTarget = backButton.parentNode;
+      document.getElementById("complete-list").removeChild(deleteTarget);
+
+      // テキスト取得
+      const text = backButton.parentNode.firstElementChild.innerText;
+      createFromInCompleteList(text);
+    });
+
+    addTarget.appendChild(li);
+    addTarget.appendChild(backButton);
+
+    document.getElementById("complete-list").appendChild(addTarget);
+  });
+
+  // 　削除ボタン
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    // 未完了リストから削除
+    deleteFromInCompleteList(deleteButton.parentNode);
+  });
+
+  // divに子要素を追加
+  div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
+
+  // 未完了リストに追加
+  document.getElementById("incomplete-list").appendChild(div);
+};
+
+// 追加ボタン
+document
+  .getElementById("add-button")
+  .addEventListener("click", () => onClickAdd());
